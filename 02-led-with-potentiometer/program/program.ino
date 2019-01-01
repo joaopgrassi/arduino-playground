108dc535e6cbc87ef51f74146cd0f5074c87f4cd
Joao Grassi <joaop.grassi@gmail.com>
2019-01-02 00:23:18 +0100

    Changed to use map function and removed not relevant logs.

--- 02-led-with-potentiometer/program/program.ino
+++ 02-led-with-potentiometer/program/program.ino
@@ -6,9 +6,6 @@ int redLedPin = 5;
 // holds the value read from the potentiometer
 int potReadValue;
 
-// value from 0 - 5
-float arduinoVoltage;
-
 // value from 0 - 255
 float ledVoltage = 0;
 
@@ -22,26 +19,11 @@ void loop() {
 
   potReadValue = analogRead(potPin);
   Serial.print("The value read from the Potentiometer is: ");
-  Serial.println(potReadValue);  
+  Serial.println(potReadValue);
 
-  delay(250);
-
-  arduinoVoltage = calculateArduinoVoltageFromPotentiometer(potReadValue);
-  Serial.print("The value read from the Potentiometer in 'Arduino Voltage' is: ");
-  Serial.println(arduinoVoltage);
-
-  ledVoltage = calculateLedVoltageFromPotentiometer(potReadValue);
-
+  ledVoltage = map(potReadValue, 0, 1023, 0, 255);
   Serial.print("The value about to be set on the LED is: ");
   Serial.println(ledVoltage);
 
   analogWrite(redLedPin, ledVoltage);  
-}
-
-float calculateArduinoVoltageFromPotentiometer(int value) {
-  return ((float)5/1023) * value;
-}
-
-float calculateLedVoltageFromPotentiometer(int value) {
-  return ((float)255/1023) * value;
 }
